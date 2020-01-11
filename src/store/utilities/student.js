98c00a66a332764484cea22ecc8acb072dcf1d34
Ethan <esam5556@gmail.com>
2020-01-11 17:41:03 -0500

    Added Student View

---
+++ src/store/utilities/student.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+
+class singleStudent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+            firstName: '',
+            lastName: '',
+            email: '',
+            imageURL: '',
+            isRegistered: false,
+            gpa: 0.0
+        }
+
+        // this.editStudent = this.editStudent.bind(this);
+        // this.deleteStudent = this.removeTrack.bind(this);
+    }
+
+
+    renderDisplay() {
+
+    }
+
+    addToCampus() {
+        if (this.state.isRegistered === false) {
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            )
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div className="studentView">
+                <h1>Show Student</h1>
+
+                <h2>{this.state.firstName} {this.state.lastName}</h2>
+                <button onclick="myFunction()">Add to Campus</button>
+
+                {this.renderDisplay()}
+            </div>
+        );
+    }
+}
+
+
+
+
+export default singleStudent;
